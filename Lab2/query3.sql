16b61e7040e5b8d9f2c29a5a8b1325c9bcab8cf9
Jack <luongtrantronghuy@gmail.com>
2022-10-24 08:36:40 -0700

    removed column

--- Lab2/query3.sql
+++ Lab2/query3.sql
@@ -1,5 +1,5 @@
 -- Query 3
-SELECT DISTINCT m.name as memberName, m.joinDate as memberJoinDate, m.memberID
+SELECT DISTINCT m.name as memberName, m.joinDate as memberJoinDate
 FROM Members m, Characters c
 WHERE m.joinDate IS NOT NULL
 AND (m.memberID) NOT IN (SELECT t.ownerMemberID FROM Things t WHERE t.ownerMemberID IS NOT NULL)
