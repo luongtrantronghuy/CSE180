77f3c9fd58592624d6fce300c4e6ebc0baa41e9c
Jack <luongtrantronghuy@gmail.com>
2022-10-23 22:11:09 -0700

    worked query 2

--- Lab2/script.sql
+++ Lab2/script.sql
@@ -1,12 +1,20 @@
--- SELECT * FROM Rooms WHERE SUBSTR(roomDescription, 2, 1) = 'w'
+-- SELECT * FROM Monsters m WHERE m.wasDefeated = false
 
--- SELECT * FROM Things WHERE cost < 12
+-- SELECT * FROM Battles
 
 -- Query 1
-SELECT t.initialRoomID as roomID, r.roomDescription as initialRoomDescription, t.thingID, t.cost
-FROM Rooms r, Things t
-WHERE roomID = ANY (SELECT t.initialRoomID 
-                    FROM Things 
-                    WHERE SUBSTR(r.roomDescription, 2, 1) = 'w') 
-                AND cost < 12
-ORDER BY initialRoomDescription, cost DESC
+-- SELECT t.initialRoomID as roomID, r.roomDescription as initialRoomDescription, t.thingID, t.cost
+-- FROM Rooms r, Things t
+-- WHERE roomID = ANY (SELECT t.initialRoomID 
+--                     FROM Things 
+--                     WHERE SUBSTR(r.roomDescription, 2, 1) = 'w') 
+--                 AND cost < 12
+-- ORDER BY initialRoomDescription, cost DESC
+
+-- Query 2
+SELECT DISTINCT m.name as monsterName
+FROM Monsters m, Battles b
+WHERE m.wasDefeated = false 
+AND m.monsterID = ANY (SELECT monsterID
+                       FROM Battles a
+                       WHERE a.monsterBattlePoints < a.characterBattlePoints)
