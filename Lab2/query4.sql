3524d8397448f305a02596ff917e77983a79e8c6
Jack <luongtrantronghuy@gmail.com>
2022-10-24 08:32:23 -0700

    found 3 tuples, need check

---
+++ Lab2/query4.sql
@@ -0,0 +1,11 @@
+-- Query 4
+SELECT DISTINCT m.name as theMonster, c.name as theCharacter, b.characterRole as theRole
+FROM Monsters m, Characters c, Members mem, Roles r, Battles b
+WHERE b.characterRole != 'knight'
+AND (b.characterMemberID, b.characterRole) = (mem.memberID, c.role)
+AND b.monsterID = m.monsterID
+AND c.memberID = mem.memberID
+AND (mem.expirationDate >= '2022-10-12')
+AND r.role = b.characterRole
+AND c.currentMoney > r.initialMoney
+AND m.name IS NOT NULL
