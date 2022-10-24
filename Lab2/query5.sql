470a8679245c32f110a248568f3b4596e224a121
Jack <luongtrantronghuy@gmail.com>
2022-10-24 11:05:51 -0700

    worked query 5

---
+++ Lab2/query5.sql
@@ -0,0 +1,6 @@
+-- Query 5
+SELECT DISTINCT r1.roomID as eastRoomID, r1.roomDescription as eastRoomDescription,
+                r2.roomID as westRoomID, r2.roomDescription as westRoomDescription
+FROM Rooms r1, Rooms r2
+WHERE r1.roomID = r2.eastNext
+AND r2.roomID = r1.westNext
