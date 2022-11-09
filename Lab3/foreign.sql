17b7d82e6b2efea92f5e2b77c1b1d0dbf4846908
Jack <luongtrantronghuy@gmail.com>
2022-11-09 14:22:23 -0800

    init foreign.sql

---
+++ Lab3/foreign.sql
@@ -0,0 +1,18 @@
+-- Constraint 1
+ALTER TABLE Battles
+ADD CONSTRAINT constraint1
+FOREIGN KEY Battles(monsterID) REFERENCES Monsters(monsterID)
+    ON UPDATE CASCADE
+
+-- Constraint 2
+ALTER TABLE Battles
+ADD CONSTRAINT constraint2
+FOREIGN KEY Battles(characterMemberID, characterRole) REFERENCES Characters(memberID, role)
+    ON DELETE CASCADE
+
+-- Constraint 3
+ALTER TABLE Things
+ADD CONSTRAINT constraint3
+FOREIGN KEY Things(ownerMemberID, ownerRole) REFERENCES Characters(memberID, role)
+    ON DELETE SET NULL
+    ON UPDATE CASCADE
