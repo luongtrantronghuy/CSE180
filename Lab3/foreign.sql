2fdef106a182c79c398f53adf0416e98d7c27cc1
Jack <luongtrantronghuy@gmail.com>
2022-11-10 17:39:42 -0800

    done unit test

--- Lab3/foreign.sql
+++ Lab3/foreign.sql
@@ -1,18 +1,18 @@
 -- Constraint 1
 ALTER TABLE Battles
-ADD CONSTRAINT constraint1
-FOREIGN KEY Battles(monsterID) REFERENCES Monsters(monsterID)
+ADD CONSTRAINT foreign_constraint1
+FOREIGN KEY (monsterID) REFERENCES Monsters(monsterID)
     ON UPDATE CASCADE;
 
 -- Constraint 2
 ALTER TABLE Battles
-ADD CONSTRAINT constraint2
-FOREIGN KEY Battles(characterMemberID, characterRole) REFERENCES Characters(memberID, role)
+ADD CONSTRAINT foreign_constraint2
+FOREIGN KEY (characterMemberID, characterRole) REFERENCES Characters(memberID, role)
     ON DELETE CASCADE;
 
 -- Constraint 3
 ALTER TABLE Things
-ADD CONSTRAINT constraint3
-FOREIGN KEY Things(ownerMemberID, ownerRole) REFERENCES Characters(memberID, role)
+ADD CONSTRAINT foreign_constraint3
+FOREIGN KEY (ownerMemberID, ownerRole) REFERENCES Characters(memberID, role)
     ON DELETE SET NULL
     ON UPDATE CASCADE;
